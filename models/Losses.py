ca4867cd7893d955612d5401ba1a92f123ef8b18
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-04-29 15:30:26 +0200

    minor

--- models/Losses.py
+++ models/Losses.py
@@ -59,7 +59,7 @@ class GANLoss:
         zmean, zlsig = latent[:, :l//2], latent[:, l//2:]
         kl = 0.5 * torch.sum(zlsig.exp() - zlsig + zmean.pow(2) - 1, dim=1)
 
-        kl = betas[0]*torch.clamp(kl, min=0.01, max=1000)
+        kl = betas[0]*torch.clamp(kl, min=0.01)
 
         for i, n in enumerate(noise):
             if n is None:
@@ -68,7 +68,7 @@ class GANLoss:
             b, c, h, w = n.size()
             mean = n[:, :c//2, :, :].view(b, -1)
             sig = n[:, c//2:, :, :].view(b, -1)
-            kl += betas[i+1] * torch.clamp(0.5 * torch.sum(sig.exp() - sig + mean.pow(2) - 1, dim=1), min=0.01, max=1000)
+            kl += betas[i+1] * torch.clamp(0.5 * torch.sum(sig.exp() - sig + mean.pow(2) - 1, dim=1), min=0.01)
 
         return kl.mean().to(latent.device)
 
