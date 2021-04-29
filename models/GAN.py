fe31de08b796f1e31d34ccb359022fa1d612ea74
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-04-29 11:06:31 +0200

    minor

--- models/GAN.py
+++ models/GAN.py
@@ -594,7 +594,7 @@ class StyleGAN:
 
         # Change this implementation for making it compatible for relativisticGAN
         recon_loss = self.loss.reconstruction_loss(reconstruction, recon_target)
-        kl_loss = 0.000001 * self.loss.kl_loss(z_distr, noise_distr)
+        kl_loss = self.loss.kl_loss(z_distr, noise_distr)
         adverserial_loss = self.loss.gen_loss(real_samples, reconstruction, depth, alpha)
 
         loss = recon_loss + kl_loss + adverserial_loss
