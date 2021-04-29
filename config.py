99b75fb0816dd503b76731472feab59779545130
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-04-29 11:46:25 +0200

    minor

--- config.py
+++ config.py
@@ -88,7 +88,7 @@ cfg.model.dis.blur_filter = [1, 2, 1]
 # Options for Generator Optimizer
 # ---------------------------------------------------------------------------- #
 cfg.model.g_optim = CN()
-cfg.model.g_optim.learning_rate = 0.003
+cfg.model.g_optim.learning_rate = 0.0003
 cfg.model.g_optim.beta_1 = 0
 cfg.model.g_optim.beta_2 = 0.99
 cfg.model.g_optim.eps = 1e-8
@@ -97,7 +97,7 @@ cfg.model.g_optim.eps = 1e-8
 # Options for Discriminator Optimizer
 # ---------------------------------------------------------------------------- #
 cfg.model.d_optim = CN()
-cfg.model.d_optim.learning_rate = 0.003
+cfg.model.d_optim.learning_rate = 0.0003
 cfg.model.d_optim.beta_1 = 0
 cfg.model.d_optim.beta_2 = 0.99
 cfg.model.d_optim.eps = 1e-8
@@ -106,7 +106,7 @@ cfg.model.d_optim.eps = 1e-8
 # Options for Encoder Optimizer
 # ---------------------------------------------------------------------------- #
 cfg.model.e_optim = CN()
-cfg.model.e_optim.learning_rate = 0.003
+cfg.model.e_optim.learning_rate = 0.0003
 cfg.model.e_optim.beta_1 = 0
 cfg.model.e_optim.beta_2 = 0.99
 cfg.model.e_optim.eps = 1e-8
