425b740895e06248e2e4bc77b06f26abe9e26d2b
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-05-10 18:09:09 +0200

    minor

---
+++ scripts/run_siglaplace_2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --time=14:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -C TitanX
+#SBATCH --gres=gpu:1
+
+module load cuda10.0/toolkit
+module load cuDNN/cuda10.0
+
+source /home/mms496/.bashrc
+
+cd /home/mms496/StyleVAE_Experiments/stylegan
+
+if [ -d "/home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_2" ] 
+then
+    echo $$
+	mkdir oo`echo $$`
+	cd oo`echo $$` 
+	cp -R /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_2 .
+	rm -rf /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_2
+	cd /home/mms496/StyleVAE_Experiments/stylegan
+
+else 
+	echo 'no problem'
+fi
+
+
+
+
+python -u /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch/train.py --start_depth 5 --config /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch/configs/ffhq_128_siglaplace_2.yaml
+
+wait          # wait until programs are finished
+
+echo $$
+mkdir o`echo $$`
+cd o`echo $$`
+
+cp -R /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_2 .
+rm -rf /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_2
