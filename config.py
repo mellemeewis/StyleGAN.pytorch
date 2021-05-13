03005d1b2a85ccc0e8534c91bd5c924f079fc39e
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-05-13 10:49:02 +0200

    adverserial

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ cfg.feedback_factor = 10
 cfg.checkpoint_factor = 10
 cfg.use_discriminator = False
 cfg.use_sleep = True
-cfg.use_advererial = True
+cfg.use_adverserial = True
 
 cfg.noise_channel_dropout = 0.
 
