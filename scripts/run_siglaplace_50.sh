2010b83be034a4ba56ae0dc05cd244222bcbf61b
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-05-15 12:29:45 +0200

    adverserial

--- scripts/run_siglaplace_50.sh
+++ scripts/run_siglaplace_50.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=14:00:00
+#SBATCH --time=48:00:00
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH -C TitanX
@@ -12,13 +12,13 @@ source /home/mms496/.bashrc
 
 cd /home/mms496/StyleVAE_Experiments/stylegan
 
-if [ -d "/home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_50" ] 
+if [ -d "/home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_50_1" ] 
 then
     echo $$
 	mkdir oo`echo $$`
 	cd oo`echo $$` 
-	cp -R /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_50 .
-	rm -rf /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_50
+	cp -R /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_50_1 .
+	rm -rf /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_50_1
 	cd /home/mms496/StyleVAE_Experiments/stylegan
 
 else 
@@ -28,7 +28,7 @@ fi
 
 
 
-python -u /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch/train.py --start_depth 5 --config /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch/configs/ffhq_128_siglaplace_50.yaml
+python -u /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch/train.py --start_depth 5 --config /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch/configs/ffhq_128_siglaplace_50_1.yaml
 
 wait          # wait until programs are finished
 
@@ -36,5 +36,5 @@ echo $$
 mkdir o`echo $$`
 cd o`echo $$`
 
-cp -R /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_50 .
-rm -rf /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_50
+cp -R /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_50_1 .
+rm -rf /home/mms496/StyleVAE_Experiments/stylegan/output_siglaplace_50_1
