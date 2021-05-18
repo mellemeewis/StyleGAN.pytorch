06408c8de7f769205357d61e8082e003bf9784e5
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-05-18 13:06:55 +0200

    minor

--- models/GAN.py
+++ models/GAN.py
@@ -664,27 +664,29 @@ class StyleGAN:
         b = real_samples.size()[0]
         # generate a batch of samples
 
-        sample_z, sample_n = self.sample_latent(b, depth)
-        gen_out = self.gen(sample_z, sample_n[::-1], depth, alpha, mode='reconstruction').detach()
-        fake_samples = self.sample_images(gen_out, self.recon_loss).detach()
+        for i in range(5):
+            sample_z, sample_n = self.sample_latent(b, depth)
+            gen_out = self.gen(sample_z, sample_n[::-1], depth, alpha, mode='reconstruction').detach()
+            fake_samples = self.sample_images(gen_out, self.recon_loss).detach()
 
-        z_recon_real, noise_recon_real = self.encoder(real_samples, depth)
-        z_recon_fake, noise_recon_fake = self.encoder(fake_samples, depth)
+            z_recon_real, noise_recon_real = self.encoder(real_samples, depth)
+            z_recon_fake, noise_recon_fake = self.encoder(fake_samples, depth)
 
 
-        real_loss = self.loss.kl_loss(z_recon_real, noise_recon_real)
-        fake_loss = self.loss.enc_as_dis_loss(z_recon_fake, noise_recon_fake, sample_z, sample_n)
+            real_loss = self.loss.kl_loss(z_recon_real, noise_recon_real)
+            fake_loss = self.loss.enc_as_dis_loss(z_recon_fake, noise_recon_fake, sample_z, sample_n)
 
-        real_total = real_loss[0] + real_loss[1] + real_loss[2] + real_loss[3] + real_loss[4] + real_loss[5] + real_loss[6] 
-        fake_total = fake_loss[0] + fake_loss[1] + fake_loss[2] + fake_loss[3] + fake_loss[4] + fake_loss[5] + fake_loss[6] 
-        dis_loss = real_total + fake_total
+            real_total = real_loss[0] + real_loss[1] + real_loss[2] + real_loss[3] + real_loss[4] + real_loss[5] + real_loss[6] 
+            fake_total = fake_loss[0] + fake_loss[1] + fake_loss[2] + fake_loss[3] + fake_loss[4] + fake_loss[5] + fake_loss[6] 
+            dis_loss = real_total + fake_total
 
-        # optimize discriminator
-        self.encoder_optim.zero_grad()
-        dis_loss.backward()
-        nn.utils.clip_grad_norm_(self.encoder.parameters(), max_norm=1.)
+            # optimize discriminator
+            self.encoder_optim.zero_grad()
+            dis_loss.backward()
+            nn.utils.clip_grad_norm_(self.encoder.parameters(), max_norm=1.)
 
-        self.encoder_optim.step()
+            self.encoder_optim.step()
+            print(real_loss, fake_loss)
 
         return dis_loss
 
@@ -897,7 +899,6 @@ class StyleGAN:
 
                     # optimize the discriminator:
                     dis_loss = self.update_enc_as_discriminator(images, current_depth, alpha) if self.update_encoder_as_discriminator else 0
-
 
                     # optimize the generator:
                     kl_loss, recon_loss = self.vae_phase(images, current_depth, alpha) if self.use_vae else 0, 0
